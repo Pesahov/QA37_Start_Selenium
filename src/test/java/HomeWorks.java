1035c9103dfe770bcecff79ec3f4d5c0197fe07a
Shilol Pesahov <pesahov777@gmail.com>
2023-03-07 10:12:26 +0200

    Index

---
+++ src/test/java/HomeWorks.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class HomeWorks {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        //wd = new ChromeDriver("https://telranedu.web.app/login");
+
+    }
+}
